05ac060308094d44e120548ae6af0e8477a9de21
GurvanGGLV <ggurvan@yahoo.fr>
2021-09-29 17:32:18 +0200

    création de la classe récepteur multi trajets

---
+++ src/transmetteurs/RecepteurMultiTrajets.java
@@ -0,0 +1,28 @@
+package transmetteurs;
+
+import information.Information;
+import information.InformationNonConformeException;
+
+/**
+ * Cette classe permet de récupérer le signalTi réalisé par la classe EmetteurMultiTrajets et va le traiter de manière à récupérer le signal
+ * analogique initial.
+ * @author ggurv
+ *
+ */
+public class RecepteurMultiTrajets extends Transmetteur<Float,Boolean> {
+
+	
+	
+	public void recevoir(Information<Float> information) throws InformationNonConformeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void emettre() throws InformationNonConformeException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	
+}
