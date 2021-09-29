bd017291fde7803c7eaca291b71f99fe951e947b
GurvanGGLV <ggurvan@yahoo.fr>
2021-09-29 17:44:40 +0200

    update emetteurmt

--- src/transmetteurs/EmetteurMultiTrajets.java
+++ src/transmetteurs/EmetteurMultiTrajets.java
@@ -29,54 +29,60 @@ public class EmetteurMultiTrajets extends Transmetteur<Float,Float>{
 	public void recevoir(Information<Float> information) throws InformationNonConformeException {
 		
 		// on récupère l'information 
-		informationRecue = information;
+		this.informationRecue = information;
+		this.emettre(); // on emet ensuite afin d'obtenir le nouveau signal
+	}
+
+	// on emet le signal
+	public void emettre() throws InformationNonConformeException {
+
 		// on initialise la liste qui contiendra le multi trajet
 		informationTi = new Information<Float>();
-		
-		// on rajoute l'information utile puis un nombre de 0 égale à la valeur du plus grand décalage
-		for(int i=0 ; i<(informationRecue.nbElements()+this.getTauMax()) ; i++) {
-			if(i<informationRecue.nbElements()) { // tant qu'on est dans l'information utile on l'écrit
+
+		// on rajoute l'information utile puis un nombre de 0 égale à la valeur du plus
+		// grand décalage
+		for (int i = 0; i < (informationRecue.nbElements() + this.getTauMax()); i++) {
+			if (i < informationRecue.nbElements()) { // tant qu'on est dans l'information utile on l'écrit
 				informationTi.add(informationRecue.iemeElement(i));
 			} else { // quand on rentre dans le décalage on entre des 0
 				informationTi.add(0f);
 			}
 		}
-		
-		// on veut maintenant respecter r(t) = s(t) +Aks(t-Tk) + b(t) , k appartenant à [|0;5|]
-		
-		// alphaCourant va nous servir à récupérer le alpha correspondant au décalage observé
-		int alphaCourant = 0;		
-		
+
+		// on veut maintenant respecter r(t) = s(t) +Aks(t-Tk) + b(t) , k appartenant à
+		// [|0;5|]
+
+		// alphaCourant va nous servir à récupérer le alpha correspondant au décalage
+		// observé
+		int alphaCourant = 0;
+
 		for (int tau : listTaus) { // on va récupérer le signal initial, et le rajouter par décalage
 			// dans le signal mutli trajet
-			for(int pos=0 ; pos<informationTi.nbElements() ; pos++) {
-				if(pos>=tau && pos<informationTi.nbElements()) { // quand l'on atteint le premier décalage
+			for (int pos = 0; pos < informationTi.nbElements(); pos++) {
+				if (pos >= tau && pos < informationTi.nbElements()) { // quand l'on atteint le premier décalage
 					// on récupère la valeur de l'échantillon actuel
 					float echCourant = informationTi.iemeElement(pos);
 					// on récupère le premier echantillon équivalent dans le signal recu de base
-					float echEq = informationRecue.iemeElement(pos-tau); // le -tau permet de récupérer
+					float echEq = informationRecue.iemeElement(pos - tau); // le -tau permet de récupérer
 					// le premier échantillon du signal recu
 					float alpha = listAlphas.get(alphaCourant);
-					
+
 					// on réaliser l'opération
-					float echTi = echCourant + (alpha*echEq);
-					
-					// et on remplace à l'endroit où l'on est par l'échantillon initial + l'échantillon du signal
+					float echTi = echCourant + (alpha * echEq);
+
+					// et on remplace à l'endroit où l'on est par l'échantillon initial +
+					// l'échantillon du signal
 					// recu * la nouvelle amplitude (0<=alpha<=1)
 					informationTi.setIemeElement(pos, echTi);
 				}
 			}
 			alphaCourant++;
 		}
-		this.emettre(); // on emet ensuite le nouveau signal
-	}
 
-	// on emet le signal
-	public void emettre() throws InformationNonConformeException {
-		for(DestinationInterface<Float> destinationConnectee : destinationsConnectees) {
+		for (DestinationInterface<Float> destinationConnectee : destinationsConnectees) {
 			destinationConnectee.recevoir(informationTi);
 		}
-		this.informationEmise=informationTi;
+		this.informationEmise = informationTi;
 	}
 	
 	/**
--- src/transmetteurs/RecepteurMultiTrajets.java
+++ src/transmetteurs/RecepteurMultiTrajets.java
@@ -1,17 +1,28 @@
 package transmetteurs;
 
+import java.util.ArrayList;
+
 import information.Information;
 import information.InformationNonConformeException;
 
 /**
- * Cette classe permet de récupérer le signalTi réalisé par la classe EmetteurMultiTrajets et va le traiter de manière à récupérer le signal
- * analogique initial.
+ * Cette classe récupère l'information multi trajets créée à partir de la classe EmetteutMultiTrajets.
+ * Elle va la traiter de manière à régénérer l'information initiale qui était informationAnalogique
  * @author ggurv
  *
  */
-public class RecepteurMultiTrajets extends Transmetteur<Float,Boolean> {
+public class RecepteurMultiTrajets extends Transmetteur<Float,Float> {
 
+	private ArrayList<Integer> listTaus;
+	private ArrayList<Float> listAlphas;
+	protected Information<Float> informationTiTraitee;
 	
+	public RecepteurMultiTrajets(ArrayList<Integer> taus, ArrayList<Float> alphas) {
+		// faire une classe pour des taus et alphas invalides
+		this.listTaus = taus;
+		this.listAlphas = alphas;
+		informationTiTraitee = null;
+	}
 	
 	public void recevoir(Information<Float> information) throws InformationNonConformeException {
 		// TODO Auto-generated method stub
