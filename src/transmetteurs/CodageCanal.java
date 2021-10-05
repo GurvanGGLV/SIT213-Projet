ade22eb53aeb18d587b7b2b141fcb89498442b8b
Ezeyre <91015032+Ezeyre@users.noreply.github.com>
2021-10-05 23:35:55 +0200

    ajout commentaire
    commentaires de méthodes de classes etc

--- src/transmetteurs/CodageCanal.java
+++ src/transmetteurs/CodageCanal.java
@@ -1,5 +1,21 @@
 package transmetteurs;
 
+/**
+ * Nom de classe 			: CodageCanal
+ * 
+ * Description 				: Cette classe se compose des methodes recevoir() et emettre() ainsi que 
+ * 							  des methodes de codage du canal de l'information. Elle a pour but de formater l'information numerique
+ * 							  en une nouvelle information analogique codée.
+ * 
+ * Version 					: 1.0
+ * 
+ * Date 					: 29/09/2021
+ * 
+ * Copyright 				: Gurvan, Christopher, Alexandre, Aurelien Promotion 2023 FIP 2A
+ * 
+ */
+
+
 import destinations.DestinationInterface;
 import information.Information;
 import information.InformationNonConformeException;
@@ -18,6 +34,13 @@ public class CodageCanal extends Transmetteur<Boolean,Boolean> {
 		this.emettre();
 	}
 
+	/** 
+	 * 
+	 * La methode emettre() de cette classe formate l'information selon la table de correspondance associée.
+	 * Elle instancie un attribut <informationCodee> qui sera envoyé au prochain élément de la chaine
+	 * de transmission.
+	 * 
+	 */
 	
 	public void emettre() throws InformationNonConformeException {
 		
--- src/transmetteurs/DecodageCanal.java
+++ src/transmetteurs/DecodageCanal.java
@@ -1,5 +1,20 @@
 package transmetteurs;
 
+/**
+ * Nom de classe 			: DecodageCanal
+ * 
+ * Description 				: Cette classe se compose des methodes recevoir() et emettre() ainsi que 
+ * 							  d'une methode de decodage de l'information. Elle a pour but deformater l'information numerique
+ * 							  codée en une information analogiqu.
+ * 
+ * Version 					: 1.0
+ * 
+ * Date 					: 19/09/2021
+ * 
+ * Copyright 				: Gurvan, Christopher, Alexandre, Aurelien Promotion 2023 FIP 2A
+ * 
+ */
+
 import java.util.Iterator;
 
 import destinations.DestinationInterface;
@@ -25,24 +40,39 @@ public class DecodageCanal extends Transmetteur<Boolean,Boolean> {
 		
 	}
 
+	/** 
+	 * La méthode emettre() de cette classe deformate l'information numérique après décodage de celle ci par 
+	 * le transmetteur adapté.
+	 * Elle transforme ainsi cette information en l'information numérique utile.
+	 * L'attribut <informationDecodee> est instancié pour stocker les valeurs correspondantes des bits.
+	 * Elle sera ensuite envoyée à l'élément destination de la chaine de transmission.
+	 * 
+	 */
 	
 	public void emettre() throws InformationNonConformeException {
 		
 		informationDecodee = new Information<Boolean>();
 		
-	
+		// la méthode has.next() permet de parcourir la trame d'information numérique.
 		
 		for(Iterator<Boolean> iterator = informationRecue.iterator() ; iterator.hasNext();) 
 		{
+			
 			
 			Boolean bit1 = iterator.next(); // etude sur le premier bit 
 			Boolean bit2 = iterator.next();	// etude sur le second bit
 			Boolean bit3 = iterator.next(); // etude sur le troisieme bit 
 			
 			Boolean[] tabBit = {bit1 , bit2 , bit3};
+			
 			// sachant qu'on a les 3 bits voulus, on les convertit en string pour les comparer
 			
 			String tabBitString = Arrays.toString(tabBit);
+			
+			// la structure "switch case" nous permettra d'effectuer la correspondance entre les
+			// valeurs de <tabBitString> et les paramètres conditionnels.
+			// Selon la correspondance des trois bits et les paramètres conditionnels, on ajoute
+			// à <tabBitString> la valeur du bit associé, 0 ou 1.
 			
 			switch(tabBitString) {
 			case  "[true, false, true]":
